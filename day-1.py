e6f0e1905d90d46163fd19fd731f8cace5d0269b
Kati Baker <kati.baker.wv@gmail.com>
2022-12-01 00:37:59 -0500

    day 1 complete

--- day-1.py
+++ day-1.py
@@ -2,7 +2,7 @@
 Advent of Code - Day 1: ???
 Helper functions to reuse for puzzles - thanks Danielle Lucek!
 """
-import re
+import numpy
 from collections import Counter
 from typing import List
 
@@ -21,6 +21,29 @@ def input_as_ints(filename:str) -> List[int]:
     line_as_int = lambda l: int(l.rstrip('\n'))
     return list(map(line_as_int, lines))
 
-FILENAME = "data/AoC-1.txt"
-file_input = input_as_string(FILENAME)
-print(file_input)
+def parse_input(file_input):
+    singleElfRations = []
+    currElf = 0
+    for i in file_input:
+        #print(i)
+        if i != '':
+            currElf += int(i)
+        if i == '':
+            #print("Elf with ration amount: " + str(currElf))
+            singleElfRations.append(currElf)
+            currElf = 0
+
+    return singleElfRations
+
+FILENAME = "data/1-input.txt"
+file_input = input_as_lines(FILENAME)
+#print(file_input)
+elfRations = parse_input(file_input)
+print("Largest ration is:", max(elfRations))
+
+### PART TWO ###
+
+elfRations.sort()
+print("Top 3 elves carrying most rations are: \n" + "1." + str(elfRations[-1]) + "\n" + "2." + str(elfRations[-2]) + "\n" + "3." + str(elfRations[-3]))
+topThree = elfRations[-1] + elfRations[-2] + elfRations[-3]
+print("Top 3 elves combined: " + str(topThree))
