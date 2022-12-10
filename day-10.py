afff35169a0105d23ae8a6c643814cd5e95c31bd
Kati Baker <kati.baker.wv@gmail.com>
2022-12-10 15:49:27 -0500

    day 10, part 2 - bug somewhere in updating the register :(

--- day-10.py
+++ day-10.py
@@ -22,42 +22,58 @@ def input_as_ints(filename:str) -> List[int]:
     return list(map(line_as_int, lines))
 
 def solution_part_1():
-    FILENAME = "data/10-input.txt"
+    FILENAME = "data/10-sample.txt"
     file_input = input_as_lines(FILENAME)
+
+    #create the grid
+    crt_screen = create_grid(40, 6)
+    #print_grid(crt_screen)
+    print("---------------------------------")
+    #flatten into one big line
+    crt_screen = [ele for crt_screen in crt_screen for ele in crt_screen]
+    
     cycle = 0
-    step = 40
-    starting_increment = 20
     register_x = 1
     signal_strength = 0
     all_signals = []
+    sprite = '#' #draw what X equals to and +1 and -1 to draw the full sprite
     for i in file_input:
         #print(i)
         if i == "noop":
             cycle += 1
+            draw_pixel(cycle, crt_screen, register_x, sprite)
         else:
             if re.search('-', i):
                 add_cycle = re.findall("-\d+", i)
                 cycle += 1
+                draw_pixel(cycle, crt_screen, register_x, sprite)
                 if check_signal(cycle) == 1:
                     signal_strength = register_x * cycle
                     all_signals.append(signal_strength)
                 cycle += 1
+                draw_pixel(cycle, crt_screen, register_x, sprite)
+                #draw pixel
                 if check_signal(cycle) == 1:
                     signal_strength = register_x * cycle
                     all_signals.append(signal_strength)
             else:
                 add_cycle = re.findall("\d+", i)
                 cycle += 1
+                draw_pixel(cycle, crt_screen, register_x, sprite)
+                #draw pixel
                 if check_signal(cycle) == 1:
                     signal_strength = register_x * cycle
                     all_signals.append(signal_strength)
                 cycle += 1
+                draw_pixel(cycle, crt_screen, register_x, sprite)
+                #draw pixel
                 if check_signal(cycle) == 1:
                     signal_strength = register_x * cycle
                     all_signals.append(signal_strength)
             
             register_x += int(add_cycle[0])
 
+    print(crt_screen)
     print(all_signals)
     total = 0
     for k in range(0,len(all_signals)):
@@ -70,6 +86,36 @@ def check_signal(cycle):
         return 1
     else:
         return -1
+
+def draw_pixel(cycle, crt_screen, register_x, sprite):
+    if (cycle == register_x) or (cycle == (register_x+1)) or ((cycle ==(register_x-1))):
+        crt_screen[cycle-1] = sprite
+
+def print_screen(crt_screen):
+    screen = []
+    row = []
+    for j in crt_screen:
+        if len(row) != 39:
+            row = ' '.join(j)
+        elif len(row) == 39:
+            screen.append(row)
+            row = []
+    print(screen)
+        
+
+def create_grid(horizontal, vertical):
+    grid = [[i for i in range(horizontal)] for n in range(vertical)]
+
+    for idx_x in range(0,vertical):
+        for idx_y in range(0, horizontal):
+            grid[idx_x][idx_y] = '.'
+
+    return grid
+
+def print_grid(grid):
+    #print grid
+    for idx_x, idx_y in enumerate(grid):
+        print(idx_x, idx_y)
 
 def solution_part_2():
     FILENAME = "data/1-input.txt"
